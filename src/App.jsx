e80d7d557c2b73371cf68e4d66181b5b30c9cfd6
Robine <Robine@macbook-robine-1.home>
2023-09-22 12:15:53 +0200

    Delivroo ended

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import favicon from "./assets/images/favicon.png";
+import deliverooIcon from "./assets/images/deliverooIcon.png";
 import "./App.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+library.add(faStar);
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App() {
   // State qui contiendra le data (la réponse du serveur)
@@ -14,10 +18,32 @@ function App() {
   const handleSelect = (meal) => {
     console.log("Meal sélectionné :", meal.title);
     const newSelect = [...select];
-    newSelect.push(meal.title, meal.price);
+
+    const existingItem = newSelect.find((item) => item.id === meal.id);
+    //Element dans le tableau? Si oui....
+    if (existingItem) {
+      //j'incrémente de 1 la quantité de l'élèment sur lequel j'ai cliqué si l'élément est dans mon tableau newSelect d'objet
+      existingItem.quantity += 1;
+      console.log(existingItem.quantity);
+    } else {
+      //sinon je l'ajoute au tableau newSelect
+      let obj = {
+        id: meal.id,
+        title: meal.title,
+        price: parseInt(meal.price),
+        quantity: 1,
+      };
+
+      newSelect.push(obj);
+      console.log(select);
+    }
     setSelect(newSelect);
-    console.log(select);
   };
+
+  let total = 0;
+  for (let i = 0; i < select.length; i++) {
+    total = total + select[i].price * select[i].quantity;
+  }
 
   useEffect(() => {
     // La fonction du useEffect ne peut pas être asynchrone, je déclare donc une fonction asynchrone à l'intérieur et je l'appelle immédiatement
@@ -46,13 +72,13 @@ function App() {
   ) : (
     <div>
       <header>
-        <div className="container">
-          <img src={favicon} alt="Logo" />
+        <div className="Container">
+          <img src={deliverooIcon} alt="Logo" />
         </div>
       </header>
-      <div className="hero">
-        <div className="container inner-hero">
-          <div className="left-part">
+      <div className="Hero">
+        <div className="Container Inner-hero">
+          <div className="Left-part">
             <h1>{data.restaurant.name}</h1>
             <p>{data.restaurant.description}</p>
           </div>
@@ -60,39 +86,40 @@ function App() {
         </div>
       </div>
       <main>
-        <div className="container inner-main">
-          <section className="col-left">
+        <div className="Container Inner-main">
+          <section className="Col-left">
             {data.categories.map((category) => {
               if (category.meals.length !== 0) {
                 return (
                   <section key={category.name}>
                     <h2>{category.name}</h2>
-                    <div className="meals-container">
+                    <div className="Meals-container">
                       {category.meals.map((meal) => {
                         return (
-                          <button
+                          <article
                             key={meal.id}
                             onClick={() => handleSelect(meal)}
                           >
-                            <article>
-                              <div>
-                                <h3>{meal.title}</h3>
-                                <p className="meal-description">
+                            <div>
+                              <h3>{meal.title}</h3>
+                              {meal.description && (
+                                <p className="Meal-description">
                                   {meal.description}
                                 </p>
-                                <span className="meal-price">
-                                  {meal.price} €
-                                </span>
-                                {meal.popular === true && (
-                                  <span>Populaire</span>
-                                )}
-                              </div>
-
-                              {meal.picture && (
-                                <img src={meal.picture} alt={meal.title} />
                               )}
-                            </article>
-                          </button>
+
+                              <span className="Meal-price">{meal.price} €</span>
+                              {meal.popular === true && (
+                                <span className="Popular-mention">
+                                  <FontAwesomeIcon icon="star" />
+                                  Populaire
+                                </span>
+                              )}
+                            </div>
+                            {meal.picture && (
+                              <img src={meal.picture} alt={meal.title} />
+                            )}
+                          </article>
                         );
                       })}
                     </div>
@@ -103,23 +130,40 @@ function App() {
               }
             })}
           </section>
-          <section className="col-right">
+          <section className="Col-right">
             <button>Valider mon panier</button>
-            {select.map((orderList, index) => {
-              return (
-                <div>
-                  <div key={index}>{orderList}</div>
-                </div>
-              );
-            })}
-            <div>
-              <div>
-                Sous-total
-                {}{" "}
-              </div>
-              <div>Frais de livraison {2.5} €</div>
+            <div className="Cart-item">
+              {select.map((elemCart, index) => {
+                return (
+                  <div key={index}>
+                    <span> {elemCart.quantity}</span>
+                    <span> {elemCart.title} </span>
+                    <span> {elemCart.price} €</span>
+                  </div>
+                );
+              })}
             </div>
-            <div>Total</div>
+
+            <div className="Cart-results">
+              <div>
+                <p>Sous-total</p>
+                <span>{total} €</span>
+              </div>
+              <div>
+                <p>Frais de livraison </p>
+                <span>{2.5} €</span>
+              </div>
+            </div>
+            <div className="Cart-total">
+              {total === 0 ? (
+                ""
+              ) : (
+                <div>
+                  <span>Total</span>
+                  <span>{total + 2.5} €</span>
+                </div>
+              )}
+            </div>
           </section>
         </div>
       </main>
